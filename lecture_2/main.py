54b929fb9a75c835f8f22a56296bd626d8af7afd
Ivan Leschinskiy <diivan622@gmail.com>
2025-11-19 00:26:58 +0300

    added input UI, almost all variables

--- lecture_2/main.py
+++ lecture_2/main.py
@@ -1,1 +1,17 @@
-print('hello world')
+def generate_profile(curent_age):
+    pass
+
+
+
+user_name = input("Hi, what is your name? ")
+birth_year_str = input("What year did you born? ")
+birth_year = int(birth_year_str)
+current_age = 2025 - birth_year
+hobbies = list()
+hobbie = input("Enter a favourite hobby or type 'stop' to finish: ")
+
+while hobbie != 'stop':
+    hobbies.append(hobbie)
+    hobbie = input("Enter a favourite hobby or type 'stop' to finish: ")
+
+life_stage = generate_profile(current_age)
